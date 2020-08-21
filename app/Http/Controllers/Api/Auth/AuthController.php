a2794c683afa595fc78a5dd4614fce7a7022beb0
mohammadsaati <mohammadsaati01@yahoo.com>
2020-08-20 23:40:22 -0700

    method login added into auth controller

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginAuthApiRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function Login()
+    public function Login(LoginAuthApiRequest $request)
     {
-        
+        if (!Auth::attempt($request))
+        {
+            return response([
+                'message' => 'Invalid password or email address' , 
+                'code' => '407'
+            ]);
+        }
+
+        $accessToken = Auth::user();
     }
 }
