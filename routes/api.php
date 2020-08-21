ba58f2b59a4491c5835dd142f4866d32ce5d505d
mohammadsaati <mohammadsaati01@yahoo.com>
2020-08-20 23:21:21 -0700

    login api added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middelware' => 'auth:api',
+    'namespace' => 'APi/Auth'
+], function () {
+    Route::get('/login' , 'AuthController@login')->name('auth.login.api');
+});
+
+Route::fallback(function() {
+    return response('page not found' , 404);
+})->name('api.404');
